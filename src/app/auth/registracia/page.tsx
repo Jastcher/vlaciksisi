e68dea7a653899ec3a8cfc0aade547003a787322
Jastcher <hugo.foltin@jstchr.tech>
2024-09-24 12:28:16 +0200

    added files

---
+++ src/app/auth/registracia/page.tsx
@@ -0,0 +1,13 @@
+// src/app/auth/registracia/page.tsx
+
+
+import Typography from '@mui/material/Typography';
+
+export const metadata = { title: "Registracia | vlak"};
+
+export default function Register() {
+  return (
+      <Typography> registracia</Typography>
+    
+  );
+}
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+// src/app/components/NavBar.tsx
+
+
+import Typography from '@mui/material/Typography';
+
+export const metadata = { title: "Registracia | vlak"};
+
+export default function NavBar() {
+  return (
+      <Typography> registracia</Typography>
+    
+  );
+}
